aea27fe8291e8b43472de602dc6bc0cf466ce835
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-07-05 19:04:17 +0500

    readme added and homepage updated

--- README.md
+++ README.md
@@ -5,5 +5,17 @@ This repository was created to ease the process of creating forms by using **Mat
 - TextField
 - Select (TextField - Single)
 - Radio
-- Checkbox
+- Checkbox (Single)
 - CheckboxList
+- DatePicker
+- Field Arrays (Text)
+- Field Arrays (Objects)
+
+# Contribution
+
+Any contribution to this repository is welcome.
+If you have any questions, please contact me at [**Email**](mailto:m.shahzebraza.97@gmail.com), [**Github**](https://github.com/mshahzebraza97) or [**LinkedIn**](https://www.linkedin.com/in/mshahzebraza/).
+
+<!-- Insert icons for Github, LinkedIn and Mail -->
+
+<!-- Insert Button to links -->
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,51 @@
 import Layout from '../components/HOC/Layout'
 import Section from '../components/HOC/Section'
 import Typography from '@mui/material/Typography'
+import { Button, Link as MuiLink, Grid } from '@mui/material'
+import NextLink from 'next/link';
+
 
 export default function Home() {
     return (
 
         <Layout>
-            <Section id="homePage"   >
-                <Typography variant="body1" >HomePage</Typography>
+            <Section id="Welcome"  >
+
+
+                <Grid
+                    container
+                    spacing={2}
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    alignContent="center"
+                    wrap="wrap"
+
+                >
+                    <Grid item xs={12} textAlign='center'>
+                        <Typography variant="body" color="initial">
+                            Kindly visit the form and feel free to use the code or contribute to the project.
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <NextLink href='/form' >
+                            <Button variant='contained' >
+                                Go To Form
+                            </Button>
+                        </NextLink>
+                    </Grid>
+
+                    <Grid item>
+                        <NextLink href='https://github.com/mshahzebraza97/mui-form' >
+                            <Button variant='contained' color='success' >
+                                Go To Repository
+                            </Button>
+                        </NextLink>
+                    </Grid>
+                </Grid>
+
+
+
             </Section>
         </Layout>
 
