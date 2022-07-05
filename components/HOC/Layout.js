8678b1bd65c114250ec185f0308ee3ee3670021b
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-07-05 19:23:19 +0500

    favicons added

--- components/HOC/Layout.js
+++ components/HOC/Layout.js
@@ -5,9 +5,12 @@ function Layout({ children }) {
     return (
         <>
             <Head>
-                <title>UI Directory</title>
-                <meta name="description" content="A Directory to store hosted components" />
-                <link rel="icon" href="/favicon.ico" />
+                <title>FormCentre</title>
+                <meta name="description" content="Formik \& MUI combo" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/my_favicon/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/my_favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/my_favicon/favicon-16x16.png" />
+                <link rel="manifest" href="/my_favicon/site.webmanifest" />
             </Head>
 
             <main >
